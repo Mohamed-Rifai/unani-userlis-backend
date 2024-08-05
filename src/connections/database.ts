ff0d611c1621541fed3dcf1b16568826b903a9db
Mohamed <rifaeeckm@gmail.com>
2024-08-05 23:37:59 +0530

    change start scripts

--- src/connections/database.ts
+++ src/connections/database.ts
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 
 export default () => {
-    const uri =  "mongodb://localhost:27017/unani_admin-panel";
+    const uri = process.env.MONGO_URI || "mongodb://localhost:27017/unani_admin-panel";
   mongoose.set("strictQuery", false);
-
+  
   mongoose 
     .connect(uri) 
     .then(() => {
       console.log( 'database connected');
     })
     .catch((err) => {
-      console.log(`Database connection failed : ${err}`);
+      console.log(`Database connection failed : ${err}`);  
     });
-};
+};   
--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -13,72 +13,72 @@ import User from '../models/user'
 
     try {
 
-        // User.create({
-        //     email:email,
-        //     password:password
-        // }).then(user => {
-        //     res.json(user)
-        // })
+        User.create({
+            email:email,
+            password:password
+        }).then(user => {
+            res.json(user)
+        })
 
 
-        User.findOne({email})
-        .then(user =>{
-            if(!user){
+//         User.findOne({email})
+//         .then(user =>{
+//             if(!user){
                  
                  
-                return res.status(404).json('Email not exist')
-            }
+//                 return res.status(404).json('Email not exist')
+//             }
 
-            const checkPassword =   (inputPassword:string,storedPassword:string) => {
-                return inputPassword === storedPassword
-            } 
+//             const checkPassword =   (inputPassword:string,storedPassword:string) => {
+//                 return inputPassword === storedPassword
+//             } 
 
-            if(checkPassword(password,user.password)){
-                const payload = {
-                    id:user.id,
-                    email:user.email
-                }
+//             if(checkPassword(password,user.password)){
+//                 const payload = {
+//                     id:user.id,
+//                     email:user.email
+//                 }
 
-                const jwtSecret = process.env.USER_JWT_SECRET
+//                 const jwtSecret = process.env.USER_JWT_SECRET
 
-                if (!jwtSecret) {
-                 throw new Error('USER_JWT_SECRET is not defined in the environment variables');
-  }
+//                 if (!jwtSecret) {
+//                  throw new Error('USER_JWT_SECRET is not defined in the environment variables');
+//   }
 
 
-                jwt.sign(
-                    payload,
-                    jwtSecret,
-                    {
-                        expiresIn:'90d'
-                    },
-                    (err,token) => {
-                        if(err){
-                            console.error('There is some error in Token',err)
-                        }else{ 
-                            res.json({
-                                success:true,
-                                email:user.email,
-                                id:user.id,
-                                token:`Bearer ${token}`,
-                            })
-                        }
-                    }
-                )
+//                 jwt.sign(
+//                     payload,
+//                     jwtSecret,
+//                     {
+//                         expiresIn:'90d'
+//                     },
+//                     (err,token) => {
+//                         if(err){
+//                             console.error('There is some error in Token',err)
+//                         }else{ 
+//                             res.json({
+//                                 success:true,
+//                                 email:user.email,
+//                                 id:user.id,
+//                                 token:`Bearer ${token}`,
+//                             })
+//                         }
+//                     }
+//                 )
          
 
 
                 
-            }else{ 
-                console.log('incorrect password');
-                return res.status(400).json('Incorrect password')
+//             }else{ 
+//                 console.log('incorrect password');
+//                 return res.status(400).json('Incorrect password')
                 
-            }
-        })
+//             }
+//         })
 
         
-    } catch (err) {
-        console.log(err);
+     } catch (err) {
+         console.log(err);
         
     }
           
