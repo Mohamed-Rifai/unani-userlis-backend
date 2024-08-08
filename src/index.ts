e4e8b1a68b8c8098c5b94ab2e39724047e29f82e
Mohamed <rifaeeckm@gmail.com>
2024-08-08 22:48:48 +0530

    update cors funtion with render url name

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-import cors from 'cors'
+
+import cors,{CorsOptions,CorsRequest} from 'cors'
 import dotenv from 'dotenv'
 
 
@@ -9,19 +10,50 @@ import patientRouter from './routes/patientRouter'
 import connectDb from './connections/database'
 
 const app = express();
+
+
+const allowedOrigins = ['http://localhost:3000', 'https://unani-userlis-backend.onrender.com'];
+
+//Create the CORS options function
+const corsOptions = ( req:CorsRequest, callback:(err:Error | null,options ?:CorsOptions)=> void)=>  {
+  let options: CorsOptions;
+
+  console.log('reached inside of cors funtion');
+  
+  const origin = req.headers.origin as string;  // Access the Origin header directly from req.headers
+
+  console.log('origin si :',origin);
+  
+  if (allowedOrigins.indexOf(origin) !== -1) {
+    options = { 
+      origin: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      credentials: true,
+      allowedHeaders: ['Content-Type', 'Authorization']
+     }; // Allow the origin
+  } else {
+    options = { origin: false }; // Disallow the origin
+  }
+  callback(null, options); // Callback expects two parameters: error and options
+};
+
+
+
 dotenv.config()
 
 
 
 
+
+
 const port = process.env.PORT || 5000;
-// CORS configuration
-const corsOptions = {
-    origin: 'http://localhost:3000', // Allow only this origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
-    credentials: true, // Allow credentials to be sent with requests
-    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
-  };
+// // CORS configuration
+// const corsOptions = {
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+//     credentials: true, // Allow credentials to be sent with requests
+//     allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+//   };
   
 //   interface EnvVariables {
 //     PORT: string;
