c7e837f93da48a7bb08262ede94e49865c4824f2
Mohamed <rifaeeckm@gmail.com>
2024-08-21 15:48:17 +0530

    new frontent url added

--- src/index.ts
+++ src/index.ts
@@ -12,17 +12,17 @@ import connectDb from './connections/database'
 const app = express();
 
 
-const allowedOrigins = ['http://localhost:3000', 'https://main--charming-cannoli-935784.netlify.app'];
+const allowedOrigins = ['http://localhost:3000', 'https://main--charming-cannoli-935784.netlify.app','https://charming-cannoli-935784.netlify.app'];
 
 //Create the CORS options function
 const corsOptions = ( req:CorsRequest, callback:(err:Error | null,options ?:CorsOptions)=> void)=>  {
   let options: CorsOptions;
 
-  console.log('reached inside of cors funtion');
+ 
   
   const origin = req.headers.origin as string;  // Access the Origin header directly from req.headers
 
-  console.log('origin si :',origin);
+ 
   
   if (allowedOrigins.indexOf(origin) !== -1) {
     options = { 
@@ -47,23 +47,7 @@ dotenv.config()
 
 
 const port = process.env.PORT || 5000;
-// // CORS configuration
-// const corsOptions = {
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
-//     credentials: true, // Allow credentials to be sent with requests
-//     allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
-//   };
-  
-//   interface EnvVariables {
-//     PORT: string;
-//     MONGO_URI: string;
-//     // JWT_SECRET: string;
-//     // CLIENT_URL: string;
-//   }
-  
-//   const env: EnvVariables = process.env as EnvVariables;
-  
+
 
 app.use(cors(corsOptions))
 app.use(express.json());
